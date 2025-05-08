1f1bd3b9929bd17fb6c536b60e8b3f0a0138ecc6
nauflabhy <n4uf4l.ku@gmail.com>
2025-05-08 15:28:36 +0700

    Membuat perulangan

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -40,4 +40,18 @@ class List
                 return;
             }
 
+            Node *previous = START;
+            Node *current = START;
+
+            while ((current != NULL) && (nim >= current->noMhs))
+            {
+                if (nim == current->noMhs)
+                {
+                    cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                    return;
+                }
+                previous = current;
+                current = current->next;
+            }
+
 }:
