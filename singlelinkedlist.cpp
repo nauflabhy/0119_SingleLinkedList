2fbb562e3662e8b85ff60d067351fc30c09253b5
nauflabhy <n4uf4l.ku@gmail.com>
2025-05-08 17:08:21 +0700

    Membuat fungsi switch case

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -119,4 +119,68 @@ int main ()
     char ch;
     while (1)
 
+    do
+    {
+        cout << "\nMenu" << endl;
+        cout << "1. Menambah data ke dalam list" << endl;
+        cout << "2. Menampilkan data dari dalam list" << endl;
+        cout << "3. Menghapus satu data dari dalam list" << endl;
+        cout << "4. Mencari data dalam list" << endl;
+        cout << "5. Keluar\n";
+        cout << "Masukkan pilihan (1-5): ";
+        cin >> ch;
+
+        switch (ch)
+        {
+        case '1':
+            mhs.addNode();
+            break;
+        case '2':
+            if (mhs.listEmpty())
+            {
+                cout << "\nList Kosong" << endl;
+            }
+            else
+            {
+                mhs.traverse();
+            }
+            break;
+        case '3':
+            cout << "\nMasukkan nomor mahasiswa yang akan dihapus : ";
+            cin >> nim;
+
+            if (mhs.delNode(nim) == false)
+            {
+                cout << "Data tidak ditemukan" << endl;
+            }
+            else
+            {
+                cout << "Data dengan nomor mahasiswa " << nim << " berhasil dihapus " << endl;
+            }
+            break;
+        case '4':
+        {
+            if (mhs.listEmpty() == true)
+            {
+                cout << "\nList Kosong\n";
+                break;
+            }
+            Node *previous, *current;
+            cout << "\nMasukkan nomor mahasiswa yang dicari : ";
+            cin >> nim;
+            if (mhs.Search(nim, &previous, &current) == false)
+                cout << "\nData tidak ditemukan" << endl;
+            else
+            {
+                cout << "\nData ditemukan" << endl;
+                cout << "\nNIM Mahasiswa: " << current->noMhs << endl;
+            }
+            break;
+        }
+        case '5':
+            break;
+        default:
+            cout << "Pilihan salah!" << endl;
+        }
+    } while (ch != '5');
 }
